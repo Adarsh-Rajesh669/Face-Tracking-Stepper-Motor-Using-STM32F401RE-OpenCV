3d91bbf3f73791e5fc1ed936509cf1fdf59d06ca
Adarsh Rajesh <adarshrajesh169@gmail.com>
2025-06-05 21:25:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ static void MX_USART2_UART_Init(void);
 #define PORT2 GPIOB
 
 uint8_t rx_buf[20];  // Buffer for UART reception
-volatile uint8_t command_ready = 0;
+
 
 // Step function
 void stepMotor(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
@@ -135,22 +135,22 @@ int main(void)
       HAL_UART_Receive(&huart2, rx_buf, sizeof(rx_buf), HAL_MAX_DELAY);
 
       if (strstr((char*)rx_buf, "RIGHT")) {
-          rotateRight(50);
-      } else if (strstr((char*)rx_buf, "LEFT")) {
+          HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);  // Turn on LED
           rotateLeft(50);
+      } else if (strstr((char*)rx_buf, "LEFT")) {
+          HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);  // Turn on LED
+          rotateRight(50);
       } else if (strstr((char*)rx_buf, "CENTER")) {
-          // Do nothing
+          HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET); // Turn off LED
+
       }
 
       memset(rx_buf, 0, sizeof(rx_buf));
   }
+
   /* USER CODE END 3 */
 }
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-    if (huart->Instance == USART2) {
-        command_ready = 1;
-    }
-}
+
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -237,14 +237,25 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA0 PA1 PA4 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4;
